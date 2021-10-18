12fd7896e3d9ee56ab9de3d96a44bc3865558661
Roshalya <rosshalya@gmail.com>
2021-10-18 15:46:50 -0500

    patient

--- components/navigation.js
+++ components/navigation.js
@@ -2,34 +2,10 @@ import Link from 'next/Link';
 import styles from './styles/Layout.module.css';
 
 const Navigation = () =>{
-    const Toggle = ()  => {
-        const navToggle= document.querySelector(".btnMenu");
-        const navMenu = document.querySelector(".menu");
-        return(
-            navToggle.addEventListener("click", ()=>{
-                navMenu.classList.toggle(".menuItems")
-            })
-        )
-    };
+    
 
     return (
 
-<<<<<<< HEAD
-                        <li className="nav-item">
-                            <Link href="/login">
-                                <a className="nav-link">Login</a>
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link href="/register">
-                                <a className="nav-link">Sign up</a>
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-=======
         <div>
             <nav className={styles.menu}>
                 <label className={styles.logo}>DENTISTA</label>
@@ -49,7 +25,6 @@ const Navigation = () =>{
             </nav>
 
         </div>
->>>>>>> 08c37d4ec534a08ccfaff7ae2773fa77915f0121
     );
 }
 
